4bbb54b282a09b37d7062e6497339efbf1cebc91
Krys_tal <kkichu444@gmail.com>
2018-12-02 23:33:56 +0530

    1133

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[eval] (
+    [Id]            INT           NOT NULL,
+    [name]          NVARCHAR (50) NULL,
+    [section]       NCHAR (10)    NULL,
+    [branch]        NCHAR (10)    NULL,
+    [date_of_birth] DATE    NULL,
+    [picture]       IMAGE         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
