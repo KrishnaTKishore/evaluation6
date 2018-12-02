7f008537c3331ded52d8c361c566d5bd9728ab48
Krys_tal <kkichu444@gmail.com>
2018-12-02 22:55:30 +0530

    2/12

--- evaluation6/Form2.Designer.cs
+++ evaluation6/Form2.Designer.cs
@@ -28,55 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.sec = new System.Windows.Forms.TextBox();
+            this.branch = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.upload = new System.Windows.Forms.Button();
+            this.select = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.img = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // id
             // 
-            this.textBox1.Location = new System.Drawing.Point(240, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.id.Location = new System.Drawing.Point(240, 61);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 20);
+            this.id.TabIndex = 0;
+            this.id.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // name
             // 
-            this.textBox2.Location = new System.Drawing.Point(240, 100);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.name.Location = new System.Drawing.Point(240, 100);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 20);
+            this.name.TabIndex = 1;
+            this.name.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // textBox3
+            // sec
             // 
-            this.textBox3.Location = new System.Drawing.Point(240, 138);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 2;
+            this.sec.Location = new System.Drawing.Point(240, 138);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(100, 20);
+            this.sec.TabIndex = 2;
             // 
-            // textBox4
+            // branch
             // 
-            this.textBox4.Location = new System.Drawing.Point(240, 180);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 3;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(240, 221);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 4;
+            this.branch.Location = new System.Drawing.Point(240, 180);
+            this.branch.Name = "branch";
+            this.branch.Size = new System.Drawing.Size(100, 20);
+            this.branch.TabIndex = 3;
             // 
             // label1
             // 
@@ -121,38 +123,111 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(163, 228);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(66, 13);
             this.label5.TabIndex = 9;
-            this.label5.Text = "label5";
+            this.label5.Text = "Date of Birth";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(415, 251);
+            this.label6.Location = new System.Drawing.Point(506, 12);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(67, 13);
             this.label6.TabIndex = 10;
-            this.label6.Text = "label6";
+            this.label6.Text = "Profile Photo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(589, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 146);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(194, 283);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(75, 23);
+            this.upload.TabIndex = 12;
+            this.upload.Text = "UPLOAD";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(498, 176);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(75, 23);
+            this.select.TabIndex = 13;
+            this.select.Text = "SELECT";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(501, 245);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 14;
+            this.show.Text = "SHOW DATABASE";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(421, 274);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(331, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // img
+            // 
+            this.img.Location = new System.Drawing.Point(589, 176);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(100, 20);
+            this.img.TabIndex = 16;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(254, 234);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(200, 20);
+            this.date.TabIndex = 17;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.img);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.branch);
+            this.Controls.Add(this.sec);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.id);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,16 +235,23 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox sec;
+        private System.Windows.Forms.TextBox branch;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox img;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker date;
     }
 }
--- evaluation6/Form2.cs
+++ evaluation6/Form2.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace evaluation6
 {
     public partial class Form2 : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Dell\Source\Repos\evaluation6\evaluation6\eval6.mdf;Integrated Security=True");
         public Form2()
         {
             InitializeComponent();
@@ -39,6 +43,40 @@ namespace evaluation6
 
         private void label5_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                img.Text = openFileDialog1.FileName;
+        }
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            Byte[] mypic = File.ReadAllBytes(openFileDialog1.FileName);
+            cmd.CommandText = "Insert into eval values('" + id.Text + "','" + name.Text + "','" + sec.Text + "','" + branch.Text + "','" + date.Text + "','" + pictureBox1.Image + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("saved");
+
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            //   string con = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True";
+            string cmd = "Select * from eval";
+            SqlDataAdapter dp = new SqlDataAdapter(cmd, con);
+            //  SqlCommandBuilder cb = new SqlCommandBuilder(dp);
+            DataTable dt = new DataTable();
+            dp.Fill(dt);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dt;
+
+            dataGridView1.DataSource = bs;
 
         }
     }
